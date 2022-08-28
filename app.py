a5a56ff7f90ad14f06bd70bb74b2c5bb81f62224
Rufuz42 <Steven.Rufus42@gmail.com>
2022-08-27 20:15:32 -0400

    Update app.py
    Added in jupyter code but getting a strange error now

--- app.py
+++ app.py
@@ -5,13 +5,27 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect, func
 import numpy as np
+import pandas as pd
 import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
-
+# From Jupyter notebook file
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect = True)
+
+# Save references to each table
+MeasurementTable = Base.classes.measurement
+StationTable = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 # Creates the server
 app = Flask(__name__)
